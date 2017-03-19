620df9fba51ba21dfdcf9825abc70909ff86b0f5
Ben Stafford <bendstafford@gmail.com>
2017-03-18 22:52:29 -0400

    Extract Normal object

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,11 +21,8 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @item = Normal.new(days_remaining, quality)
+    item.tick
   end
 
   def brie_tick
@@ -48,6 +45,16 @@ class GildedRose
     @quality += 1
     @quality += 1 if days_remaining < 10
     @quality += 1 if days_remaining < 5
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if days_remaining
+    @days_remaining
   end
 
 end
